4bde6f46543565fdaccd1ed36ce2467863b5284e
bitterButler <b.hudson.mark@gmail.com>
2023-10-05 23:14:24 +0200

    Finished the exercise fully

--- Who_is_paying.py
+++ Who_is_paying.py
@@ -9,7 +9,15 @@ names = names_string.split(",")
 # we need length of the list, for knowing what should be the random() upper-limit.
 names_length = len(names)
 
+randomChoice = random.randint(0, names_length-1)
+# lists starts indexing from 0. - So names[0] = asd, ...
+# but our randomChoice starts from 1. -> so we need to match indexing with random choosens number. This is why we use '-1' at the end of randint().
+
+PayingPerson = names[randomChoice]
+print(PayingPerson + " is going to buy the meal today!")
+
 
 # just for testing purposes
+print(randomChoice)
 print(names_length)
 print(names[3])
